3cbd1a25edac611d1c7774aa105dc1a6d79db37d
raje2411 <rraman@192.168.86.23>
2018-08-15 19:13:51 -0400

    MathQuiz Finished V1

--- src/GuiDemo/MathQuiz.java
+++ src/GuiDemo/MathQuiz.java
@@ -16,7 +16,8 @@ import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MathQuiz {
-
+	public int[] num1;
+	public int[] num2;
 	private JFrame frame;
 	private JTextField num1_1;
 	private JTextField num2_1;
@@ -35,19 +36,45 @@ public class MathQuiz {
 	private JTextField ans_5;
 	private JLabel lblMath;
 	private JButton btnAnswers;
-	private JPanel comments_1;
-	private JPanel comments_2;
-	private JPanel comments_3;
-	private JPanel comments_4;
-	private JPanel comments_5;
+	public int collect_answer_arry[], correct_answer_arry[];
+	private JLabel lblComment1;
+	private JLabel lblComment2;
+	private JLabel lblComment3;
+	private JLabel lblComment4;
+	private JLabel lblComment5;
+	public JLabel[] lblComment;
 
-	public void input_num() {
+	public void collect_answers(int answers, int position) {
+		collect_answer_arry[position] = answers;
+		return;
+	}
+
+	public void math(int num1[], int num2[]) {
+
+		for (int i = 0; i < 5; i++) {
+			correct_answer_arry[i] = num1[i] + num2[i];
+
+		}
 
 	}
 
 	public int random_num() {
 
 		return ThreadLocalRandom.current().nextInt(0, 10);
+	}
+
+	public void validate_and_print_message() {
+		
+		lblComment = new JLabel[] { lblComment1, lblComment2, lblComment3, lblComment4, lblComment5 };
+
+		for (int j = 0; j < 5; j++) {
+			if (collect_answer_arry[j] == correct_answer_arry[j]) {
+				lblComment[j].setText("CORRECT");
+			} else {
+				lblComment[j].setText("Wrong TARUN "+ num1[j] + " +  " +num2[j]+" = "+ correct_answer_arry[j]);
+			}
+		}
+
 	}
 
 	/**
@@ -61,6 +88,7 @@ public class MathQuiz {
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
+					System.out.println("hey something wrong");
 				}
 			}
 		});
@@ -70,19 +98,66 @@ public class MathQuiz {
 	 * Create the application.
 	 */
 	public MathQuiz() {
+
+		num1 = new int[5];
+		num2 = new int[5];
+		collect_answer_arry = new int[5];
+		correct_answer_arry = new int[5];
+
 		initialize();
 
-		num1_1.setText(Integer.toString(random_num()));
-		num1_2.setText(Integer.toString(random_num()));
-		num1_3.setText(Integer.toString(random_num()));
-		num1_4.setText(Integer.toString(random_num()));
-		num1_5.setText(Integer.toString(random_num()));
-		num2_1.setText(Integer.toString(random_num()));
-		num2_2.setText(Integer.toString(random_num()));
-		num2_3.setText(Integer.toString(random_num()));
-		num2_4.setText(Integer.toString(random_num()));
-		num2_5.setText(Integer.toString(random_num()));
+		num1[0] = random_num();
+		num1_1.setText(Integer.toString(num1[0]));
 
+		num1[1] = random_num();
+		num1_2.setText(Integer.toString(num1[1]));
+
+		num1[2] = random_num();
+		num1_3.setText(Integer.toString(num1[2]));
+
+		num1[3] = random_num();
+		num1_4.setText(Integer.toString(num1[3]));
+
+		num1[4] = random_num();
+		num1_5.setText(Integer.toString(num1[4]));
+
+		num2[0] = random_num();
+		num2_1.setText(Integer.toString(num2[0]));
+
+		num2[1] = random_num();
+		num2_2.setText(Integer.toString(num2[1]));
+
+		num2[2] = random_num();
+		num2_3.setText(Integer.toString(num2[2]));
+
+		num2[3] = random_num();
+		num2_4.setText(Integer.toString(num2[3]));
+
+		num2[4] = random_num();
+		num2_5.setText(Integer.toString(num2[4]));
+
+		// computing the value math() for the questions.
+		math(num1, num2);
+
+		lblComment1 = new JLabel("");
+		lblComment1.setBounds(685, 71, 242, 46);
+		frame.getContentPane().add(lblComment1);
+
+		lblComment2 = new JLabel("");
+		lblComment2.setBounds(685, 133, 242, 46);
+		frame.getContentPane().add(lblComment2);
+
+		lblComment3 = new JLabel("");
+		lblComment3.setBounds(685, 207, 242, 46);
+		frame.getContentPane().add(lblComment3);
+
+		lblComment4 = new JLabel("");
+		lblComment4.setBounds(685, 273, 242, 46);
+		frame.getContentPane().add(lblComment4);
+
+		lblComment5 = new JLabel("");
+		lblComment5.setBounds(685, 339, 242, 46);
+		frame.getContentPane().add(lblComment5);
 	}
 
 	/**
@@ -93,97 +168,97 @@ public class MathQuiz {
 		frame.setBounds(-27, -22, 955, 719);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
-		
-				num2_1 = new JTextField();
-				num2_1.setBounds(291, 67, 130, 54);
-				frame.getContentPane().add(num2_1);
-				num2_1.setColumns(10);
-		
-				ans_2 = new JTextField();
-				ans_2.setColumns(10);
-				ans_2.setBounds(517, 133, 130, 54);
-				frame.getContentPane().add(ans_2);
-		
-				ans_3 = new JTextField();
-				ans_3.setColumns(10);
-				ans_3.setBounds(517, 199, 130, 54);
-				frame.getContentPane().add(ans_3);
-		
-				num1_1 = new JTextField();
-				num1_1.setBounds(65, 67, 130, 54);
-				frame.getContentPane().add(num1_1);
-				num1_1.setColumns(10);
-		
-				num1_3 = new JTextField();
-				num1_3.setColumns(10);
-				num1_3.setBounds(65, 199, 130, 54);
-				frame.getContentPane().add(num1_3);
-		
-				num2_4 = new JTextField();
-				num2_4.setColumns(10);
-				num2_4.setBounds(291, 265, 130, 54);
-				frame.getContentPane().add(num2_4);
-		
-				num2_3 = new JTextField();
-				num2_3.setColumns(10);
-				num2_3.setBounds(289, 199, 130, 54);
-				frame.getContentPane().add(num2_3);
-		
-				ans_5 = new JTextField();
-				ans_5.setColumns(10);
-				ans_5.setBounds(517, 331, 130, 54);
-				frame.getContentPane().add(ans_5);
-		
-				num2_5 = new JTextField();
-				num2_5.setColumns(10);
-				num2_5.setBounds(289, 331, 130, 54);
-				frame.getContentPane().add(num2_5);
-		
-				num2_2 = new JTextField();
-				num2_2.setColumns(10);
-				num2_2.setBounds(289, 133, 130, 54);
-				frame.getContentPane().add(num2_2);
-		
-				ans_1 = new JTextField();
-				ans_1.setColumns(10);
-				ans_1.setBounds(519, 67, 130, 54);
-				frame.getContentPane().add(ans_1);
-		
-				num1_4 = new JTextField();
-				num1_4.setColumns(10);
-				num1_4.setBounds(67, 265, 130, 54);
-				frame.getContentPane().add(num1_4);
-		
-				num1_5 = new JTextField();
-				num1_5.setColumns(10);
-				num1_5.setBounds(65, 331, 130, 54);
-				frame.getContentPane().add(num1_5);
-		
-				num1_2 = new JTextField();
-				num1_2.setColumns(10);
-				num1_2.setBounds(65, 133, 130, 54);
-				frame.getContentPane().add(num1_2);
-		
-				ans_4 = new JTextField();
-				ans_4.setColumns(10);
-				ans_4.setBounds(519, 265, 130, 54);
-				frame.getContentPane().add(ans_4);
-				
-				num1_1.setHorizontalAlignment(JTextField.CENTER);
-				num1_2.setHorizontalAlignment(JTextField.CENTER);
-				num1_3.setHorizontalAlignment(JTextField.CENTER);
-				num1_4.setHorizontalAlignment(JTextField.CENTER);
-				num1_5.setHorizontalAlignment(JTextField.CENTER);
-				num2_1.setHorizontalAlignment(JTextField.CENTER);
-				num2_2.setHorizontalAlignment(JTextField.CENTER);
-				num2_3.setHorizontalAlignment(JTextField.CENTER);
-				num2_4.setHorizontalAlignment(JTextField.CENTER);
-				num2_5.setHorizontalAlignment(JTextField.CENTER);
-				ans_1.setHorizontalAlignment(JTextField.CENTER);
-				ans_2.setHorizontalAlignment(JTextField.CENTER);
-				ans_3.setHorizontalAlignment(JTextField.CENTER);
-				ans_4.setHorizontalAlignment(JTextField.CENTER);
-				ans_5.setHorizontalAlignment(JTextField.CENTER);
+
+		num2_1 = new JTextField();
+		num2_1.setBounds(291, 67, 130, 54);
+		frame.getContentPane().add(num2_1);
+		num2_1.setColumns(10);
+
+		ans_2 = new JTextField();
+		ans_2.setColumns(10);
+		ans_2.setBounds(517, 133, 130, 54);
+		frame.getContentPane().add(ans_2);
+
+		ans_3 = new JTextField();
+		ans_3.setColumns(10);
+		ans_3.setBounds(517, 199, 130, 54);
+		frame.getContentPane().add(ans_3);
+
+		num1_1 = new JTextField();
+		num1_1.setBounds(65, 67, 130, 54);
+		frame.getContentPane().add(num1_1);
+		num1_1.setColumns(10);
+
+		num1_3 = new JTextField();
+		num1_3.setColumns(10);
+		num1_3.setBounds(65, 199, 130, 54);
+		frame.getContentPane().add(num1_3);
+
+		num2_4 = new JTextField();
+		num2_4.setColumns(10);
+		num2_4.setBounds(291, 265, 130, 54);
+		frame.getContentPane().add(num2_4);
+
+		num2_3 = new JTextField();
+		num2_3.setColumns(10);
+		num2_3.setBounds(289, 199, 130, 54);
+		frame.getContentPane().add(num2_3);
+
+		ans_5 = new JTextField();
+		ans_5.setColumns(10);
+		ans_5.setBounds(517, 331, 130, 54);
+		frame.getContentPane().add(ans_5);
+
+		num2_5 = new JTextField();
+		num2_5.setColumns(10);
+		num2_5.setBounds(289, 331, 130, 54);
+		frame.getContentPane().add(num2_5);
+
+		num2_2 = new JTextField();
+		num2_2.setColumns(10);
+		num2_2.setBounds(289, 133, 130, 54);
+		frame.getContentPane().add(num2_2);
+
+		ans_1 = new JTextField();
+		ans_1.setColumns(10);
+		ans_1.setBounds(519, 67, 130, 54);
+		frame.getContentPane().add(ans_1);
+
+		num1_4 = new JTextField();
+		num1_4.setColumns(10);
+		num1_4.setBounds(67, 265, 130, 54);
+		frame.getContentPane().add(num1_4);
+
+		num1_5 = new JTextField();
+		num1_5.setColumns(10);
+		num1_5.setBounds(65, 331, 130, 54);
+		frame.getContentPane().add(num1_5);
+
+		num1_2 = new JTextField();
+		num1_2.setColumns(10);
+		num1_2.setBounds(65, 133, 130, 54);
+		frame.getContentPane().add(num1_2);
+
+		ans_4 = new JTextField();
+		ans_4.setColumns(10);
+		ans_4.setBounds(519, 265, 130, 54);
+		frame.getContentPane().add(ans_4);
+
+		num1_1.setHorizontalAlignment(JTextField.CENTER);
+		num1_2.setHorizontalAlignment(JTextField.CENTER);
+		num1_3.setHorizontalAlignment(JTextField.CENTER);
+		num1_4.setHorizontalAlignment(JTextField.CENTER);
+		num1_5.setHorizontalAlignment(JTextField.CENTER);
+		num2_1.setHorizontalAlignment(JTextField.CENTER);
+		num2_2.setHorizontalAlignment(JTextField.CENTER);
+		num2_3.setHorizontalAlignment(JTextField.CENTER);
+		num2_4.setHorizontalAlignment(JTextField.CENTER);
+		num2_5.setHorizontalAlignment(JTextField.CENTER);
+		ans_1.setHorizontalAlignment(JTextField.CENTER);
+		ans_2.setHorizontalAlignment(JTextField.CENTER);
+		ans_3.setHorizontalAlignment(JTextField.CENTER);
+		ans_4.setHorizontalAlignment(JTextField.CENTER);
+		ans_5.setHorizontalAlignment(JTextField.CENTER);
 
 		JLabel label = new JLabel("+");
 		label.setForeground(Color.RED);
@@ -255,32 +330,40 @@ public class MathQuiz {
 		frame.getContentPane().add(lblMath);
 
 		btnAnswers = new JButton("Click here for Answers");
+
 		btnAnswers.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+
+				// collecting the Answers data and sending to method to store in array
+
+				/*
+				 * collect_answer_arry[0] = Integer.parseInt(ans_1.getText());
+				 * collect_answer_arry[1] = Integer.parseInt(ans_2.getText());
+				 * collect_answer_arry[2] = Integer.parseInt(ans_3.getText());
+				 * collect_answer_arry[3] = Integer.parseInt(ans_4.getText());
+				 * collect_answer_arry[4] = Integer.parseInt(ans_5.getText());
+				 */
+
+				collect_answers(Integer.parseInt(ans_1.getText()), 0);
+				collect_answers(Integer.parseInt(ans_2.getText()), 1);
+				collect_answers(Integer.parseInt(ans_3.getText()), 2);
+				collect_answers(Integer.parseInt(ans_4.getText()), 3);
+				collect_answers(Integer.parseInt(ans_5.getText()), 4);
+				//
+
+				// validate_and_print_message();
+				//
+
+				// computing the value math() for the questions.
+
+				// math(num1,num2);
+				// lblComment1.setText("Yes math is processed");
+
+				validate_and_print_message();
 
 			}
 		});
 		btnAnswers.setBounds(324, 436, 249, 29);
 		frame.getContentPane().add(btnAnswers);
-
-		comments_1 = new JPanel();
-		comments_1.setBounds(692, 67, 241, 54);
-		frame.getContentPane().add(comments_1);
-
-		comments_2 = new JPanel();
-		comments_2.setBounds(692, 133, 241, 54);
-		frame.getContentPane().add(comments_2);
-
-		comments_3 = new JPanel();
-		comments_3.setBounds(692, 199, 241, 54);
-		frame.getContentPane().add(comments_3);
-
-		comments_4 = new JPanel();
-		comments_4.setBounds(692, 265, 241, 54);
-		frame.getContentPane().add(comments_4);
-
-		comments_5 = new JPanel();
-		comments_5.setBounds(692, 331, 241, 54);
-		frame.getContentPane().add(comments_5);
 	}
 }
