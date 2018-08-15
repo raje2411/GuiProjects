bca89ae1f961a783d84e9f991d1d8a3552674cb8
raje2411 <rraman@192.168.86.23>
2018-08-15 07:44:05 -0400

    Updates to import

--- src/GuiDemo/Frame1.java
+++ src/GuiDemo/Frame1.java
@@ -3,18 +3,18 @@ package GuiDemo;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
-import javax.swing.JPanel;
+//import javax.swing.JOptionPane;
+//import javax.swing.JPanel;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import javax.swing.JLabel;
-import java.awt.FlowLayout;
-import java.awt.BorderLayout;
-import javax.swing.BoxLayout;
+//import java.awt.FlowLayout;
+//import java.awt.BorderLayout;
+//import javax.swing.BoxLayout;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
-import javax.swing.LayoutStyle.ComponentPlacement;
+//import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.Color;
 
 public class Frame1 {
