5555a776ca5a8d867dacc9bb844ff26e5986b4ae
raje2411 <rraman@192.168.86.23>
2018-08-15 21:13:56 -0400

    Math quiz

--- src/GuiDemo/MathQuiz.java
+++ src/GuiDemo/MathQuiz.java
@@ -8,12 +8,18 @@ import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
 import java.awt.Color;
+
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.util.Arrays;
 import java.util.concurrent.ThreadLocalRandom;
+import javax.swing.SwingConstants;
+import java.awt.Dialog.ModalExclusionType;
+import org.eclipse.wb.swing.FocusTraversalOnArray;
+import java.awt.Component;
 
 public class MathQuiz {
 	public int[] num1;
@@ -43,6 +49,17 @@ public class MathQuiz {
 	private JLabel lblComment4;
 	private JLabel lblComment5;
 	public JLabel[] lblComment;
+	private JLabel label;
+	private JLabel label_1;
+	private JLabel label_2;
+	private JLabel label_3;
+	private JLabel label_4;
+	private JLabel label_5;
+	private JLabel label_6;
+	private JLabel label_7;
+	private JLabel label_8;
+	private JLabel label_9;
+	private JLabel lblNewLabel;
 
 	public void collect_answers(int answers, int position) {
 		collect_answer_arry[position] = answers;
@@ -64,14 +81,15 @@ public class MathQuiz {
 	}
 
 	public void validate_and_print_message() {
-		
-		lblComment = new JLabel[] { lblComment1, lblComment2, lblComment3, lblComment4, lblComment5 };
 
+		lblComment = new JLabel[] { lblComment1, lblComment2, lblComment3, lblComment4, lblComment5 };
+		//ImageIcon image = new ImageIcon(getClass().getResource("/Users/rraman/Desktop/Java_program/GuiDemo/src/GuiDemo/"  + ".gif"));
 		for (int j = 0; j < 5; j++) {
 			if (collect_answer_arry[j] == correct_answer_arry[j]) {
 				lblComment[j].setText("CORRECT");
+				//lblComment[j].setIcon(image);
 			} else {
-				lblComment[j].setText("Wrong TARUN "+ num1[j] + " +  " +num2[j]+" = "+ correct_answer_arry[j]);
+				lblComment[j].setText("Wrong TARUN " + num1[j] + " +  " + num2[j] + " = " + correct_answer_arry[j]);
 			}
 		}
 
@@ -88,7 +106,7 @@ public class MathQuiz {
 					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
-					System.out.println("hey something wrong");
+					System.out.println("hey something wrong in try catch in main function");
 				}
 			}
 		});
@@ -140,24 +158,60 @@ public class MathQuiz {
 		math(num1, num2);
 
 		lblComment1 = new JLabel("");
+		lblComment1.setIcon(null);
+		lblComment1.setBackground(Color.WHITE);
+		lblComment1.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		lblComment1.setForeground(new Color(0, 0, 0));
+		lblComment1.setHorizontalAlignment(SwingConstants.CENTER);
 		lblComment1.setBounds(685, 71, 242, 46);
 		frame.getContentPane().add(lblComment1);
 
 		lblComment2 = new JLabel("");
+		lblComment2.setIcon(null);
+		lblComment2.setBackground(Color.WHITE);
+		lblComment2.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		lblComment2.setForeground(new Color(0, 0, 0));
+		lblComment2.setHorizontalAlignment(SwingConstants.CENTER);
 		lblComment2.setBounds(685, 133, 242, 46);
 		frame.getContentPane().add(lblComment2);
 
 		lblComment3 = new JLabel("");
+		lblComment3.setIcon(null);
+		lblComment3.setBackground(Color.WHITE);
+		lblComment3.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		lblComment3.setForeground(new Color(0, 0, 0));
+		lblComment3.setHorizontalAlignment(SwingConstants.CENTER);
 		lblComment3.setBounds(685, 207, 242, 46);
 		frame.getContentPane().add(lblComment3);
 
 		lblComment4 = new JLabel("");
+		lblComment4.setIcon(null);
+		lblComment4.setBackground(Color.WHITE);
+		lblComment4.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		lblComment4.setForeground(new Color(0, 0, 0));
+		lblComment4.setHorizontalAlignment(SwingConstants.CENTER);
 		lblComment4.setBounds(685, 273, 242, 46);
 		frame.getContentPane().add(lblComment4);
 
 		lblComment5 = new JLabel("");
+		lblComment5.setIcon(null);
+		lblComment5.setBackground(Color.WHITE);
+		lblComment5.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
+		lblComment5.setForeground(new Color(0, 0, 0));
+		lblComment5.setHorizontalAlignment(SwingConstants.CENTER);
 		lblComment5.setBounds(685, 339, 242, 46);
 		frame.getContentPane().add(lblComment5);
+		
+		JButton btnAnotherQuiz = new JButton("Another Quiz ?");
+		btnAnotherQuiz.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				MathQuiz.main(new String[] {});
+
+			}
+		});
+		btnAnotherQuiz.setBounds(65, 545, 117, 29);
+		frame.getContentPane().add(btnAnotherQuiz);
+		frame.getContentPane().setFocusTraversalPolicy(new FocusTraversalOnArray(new Component[]{num1_1, lblComment4, num2_1, ans_2, ans_3, num1_3, num2_4, num2_3, ans_5, num2_5, num2_2, ans_1, num1_4, num1_5, num1_2, ans_4, label, label_1, label_2, label_3, label_4, label_5, label_6, label_7, label_8, label_9, lblNewLabel, lblMath, btnAnswers, lblComment1, lblComment2, lblComment3, lblComment5, btnAnotherQuiz}));
 	}
 
 	/**
@@ -165,6 +219,8 @@ public class MathQuiz {
 	 */
 	private void initialize() {
 		frame = new JFrame();
+		frame.getContentPane().setBackground(new Color(51, 204, 255));
+		frame.setForeground(Color.BLUE);
 		frame.setBounds(-27, -22, 955, 719);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
@@ -260,71 +316,74 @@ public class MathQuiz {
 		ans_4.setHorizontalAlignment(JTextField.CENTER);
 		ans_5.setHorizontalAlignment(JTextField.CENTER);
 
-		JLabel label = new JLabel("+");
+		label = new JLabel("+");
 		label.setForeground(Color.RED);
 		label.setFont(new Font("Lucida Grande", Font.BOLD, 22));
 		label.setBounds(224, 82, 17, 16);
 		frame.getContentPane().add(label);
 
-		JLabel label_1 = new JLabel("=");
+		label_1 = new JLabel("=");
 		label_1.setForeground(Color.RED);
 		label_1.setFont(new Font("Lucida Grande", Font.BOLD, 22));
 		label_1.setBounds(468, 86, 17, 16);
 		frame.getContentPane().add(label_1);
 
-		JLabel label_2 = new JLabel("=");
+		label_2 = new JLabel("=");
 		label_2.setForeground(Color.RED);
 		label_2.setFont(new Font("Lucida Grande", Font.BOLD, 22));
 		label_2.setBounds(466, 152, 17, 16);
 		frame.getContentPane().add(label_2);
 
-		JLabel label_3 = new JLabel("+");
+		label_3 = new JLabel("+");
 		label_3.setForeground(Color.RED);
 		label_3.setFont(new Font("Lucida Grande", Font.BOLD, 22));
 		label_3.setBounds(222, 148, 17, 16);
 		frame.getContentPane().add(label_3);
 
-		JLabel label_4 = new JLabel("=");
+		label_4 = new JLabel("=");
 		label_4.setForeground(Color.RED);
 		label_4.setFont(new Font("Lucida Grande", Font.BOLD, 22));
 		label_4.setBounds(466, 218, 17, 16);
 		frame.getContentPane().add(label_4);
 
-		JLabel label_5 = new JLabel("+");
+		label_5 = new JLabel("+");
 		label_5.setForeground(Color.RED);
 		label_5.setFont(new Font("Lucida Grande", Font.BOLD, 22));
 		label_5.setBounds(222, 214, 17, 16);
 		frame.getContentPane().add(label_5);
 
-		JLabel label_6 = new JLabel("=");
+		label_6 = new JLabel("=");
 		label_6.setForeground(Color.RED);
 		label_6.setFont(new Font("Lucida Grande", Font.BOLD, 22));
 		label_6.setBounds(468, 284, 17, 16);
 		frame.getContentPane().add(label_6);
 
-		JLabel label_7 = new JLabel("+");
+		label_7 = new JLabel("+");
 		label_7.setForeground(Color.RED);
 		label_7.setFont(new Font("Lucida Grande", Font.BOLD, 22));
 		label_7.setBounds(224, 280, 17, 16);
 		frame.getContentPane().add(label_7);
 
-		JLabel label_8 = new JLabel("=");
+		label_8 = new JLabel("=");
 		label_8.setForeground(Color.RED);
 		label_8.setFont(new Font("Lucida Grande", Font.BOLD, 22));
 		label_8.setBounds(466, 350, 17, 16);
 		frame.getContentPane().add(label_8);
 
-		JLabel label_9 = new JLabel("+");
+		label_9 = new JLabel("+");
 		label_9.setForeground(Color.RED);
 		label_9.setFont(new Font("Lucida Grande", Font.BOLD, 22));
 		label_9.setBounds(222, 346, 17, 16);
 		frame.getContentPane().add(label_9);
 
-		JLabel lblNewLabel = new JLabel("Answer");
+		lblNewLabel = new JLabel("Answer");
+		lblNewLabel.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		lblNewLabel.setForeground(new Color(0, 51, 255));
 		lblNewLabel.setBounds(734, 23, 61, 16);
 		frame.getContentPane().add(lblNewLabel);
 
-		lblMath = new JLabel("Math For Kids:");
+		lblMath = new JLabel("Math Quiz For Kids:");
+		lblMath.setFont(new Font("Lucida Grande", Font.PLAIN, 14));
 		lblMath.setForeground(Color.BLUE);
 		lblMath.setBounds(63, 6, 165, 33);
 		frame.getContentPane().add(lblMath);
